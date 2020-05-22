5d447dc478a595bcc9c20f0ff1e74b90f6859bd4
Taoz <371956576@qq.com>
2020-05-22 10:32:52 +0800

    按配置生成工程添加默认值和注释

--- src/views/CreateProject/components/AddProjFormByConf.vue
+++ src/views/CreateProject/components/AddProjFormByConf.vue
@@ -1,6 +1,6 @@
 <template>
   <el-form class="fast-proj-form" :model="form" label-position="top">
-    <el-form-item label="项目名称">
+    <el-form-item label="工程名称">
       <el-input
         size="small"
         v-model="form.project_name_zh"
@@ -9,24 +9,28 @@
       ></el-input>
     </el-form-item>
 
-    <el-form-item label="项目标识（英文）">
+    <el-form-item label="工程标识（英文）">
       <el-input size="small" v-model="form.project_name" style="width: 320px;"></el-input>
     </el-form-item>
 
     <el-form-item label="数据存储字段">
       <div class="field" v-for="(field, index) in form.tpl_input" :key="index">
+        <div class="field-title">字段名</div>
+        <el-input size="small" v-model="field.name" style="width: 300px;" placeholder="请输入字段名"></el-input>
         <el-select style="width: 120px;" v-model="field.type" size="small" placeholder="请选择">
           <el-option label="字符串" value="String(255)"></el-option>
           <el-option label="数字" value="Float"></el-option>
           <el-option label="长文本" value="Text"></el-option>
           <el-option label="媒体链接" value="Media"></el-option>
         </el-select>
-        <el-input size="small" v-model="field.name" style="width: 300px;" placeholder="请输入字段名"></el-input>
+
+        <div class="field-title">字段注释</div>
+        <el-input size="small" v-model="field.comment" style="width: 300px;"></el-input>
 
         <div class="field-title">提取规则</div>
         <el-select
           style="width: 120px;"
-          v-model="field.extract_rule_type"
+          v-model="field.extract_type"
           size="small"
           placeholder="请选择"
         >
@@ -34,8 +38,10 @@
           <el-option label="css" value="css"></el-option>
           <el-option label="正则表达式" value="regexp"></el-option>
         </el-select>
-
         <el-input size="small" v-model="field.extract_rule" style="width: 300px;"></el-input>
+
+        <div class="field-title">默认值</div>
+        <el-input size="small" v-model="field.default" style="width: 300px;"></el-input>
 
         <el-button type="text" icon="el-icon-delete" class="del-btn" @click="delField(index)"></el-button>
       </div>
@@ -62,8 +68,10 @@ export default {
           {
             name: null,
             type: "String(255)",
-            extract_rule_type: "xpath",
-            extract_rule: null
+            extract_type: "xpath",
+            extract_rule: null,
+            default: null,
+            comment: null
           }
         ]
       }
@@ -74,8 +82,10 @@ export default {
       this.form.tpl_input.push({
         name: null,
         type: "String(255)",
-        extract_rule_type: "xpath",
-        extract_rule: null
+        extract_type: "xpath",
+        extract_rule: null,
+        default: null,
+        comment: null
       });
     },
     delField(i) {
